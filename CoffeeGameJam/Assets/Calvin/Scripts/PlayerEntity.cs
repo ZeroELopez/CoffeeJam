a0313a74afee6ee6ef9c81062c781d4dc97828bc
docgilgamesh <docgilgamesh@gmail.com>
2023-09-18 22:09:36 -0400

    Attacking

--- CoffeeGameJam/Assets/Calvin/Scripts/PlayerEntity.cs
+++ CoffeeGameJam/Assets/Calvin/Scripts/PlayerEntity.cs
@@ -7,32 +7,64 @@ public class PlayerEntity : MonoBehaviour, InputController.IPlayerControllerActi
 {
     private bool isMoving = false;
     private Vector3 direction;
+    private Vector2 lastDirection;
     private InputController controls;
 
     [SerializeField]
     private float moveSpeed;
+
+    [SerializeField]
+    private BoxCollider2D attackHitbox;
+
+    [SerializeField]
+    private int attackStartUpFrames;
+
+    [SerializeField]
+    private int attackActiveFrames;
+
+    [SerializeField]
+    private int attackRecoveryFrames;
+
+    private bool isAttacking;
 
     public void OnAttackInteract(InputAction.CallbackContext context)
     {
         if(context.canceled)
         {
             //TODO: Contextual interaction and attack
-
-
+            if(!isAttacking)
+            {
+                StartCoroutine(AttackCoroutine());
+            }
         }
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
         isMoving = context.started || context.performed;
-        Vector2 readVector = context.ReadValue<Vector2>();
-        direction = IsoVectorConvert(new Vector3(readVector.x, 0, readVector.y));
+
+        if (isMoving)
+        {
+            Vector2 readVector = context.ReadValue<Vector2>();
+            direction = IsoVectorConvert(new Vector3(readVector.x, 0, readVector.y));
+
+            if(direction.x != 0)
+            {
+                lastDirection.x = Mathf.Sign(direction.x);
+            }
+
+            if(direction.z != 0)
+            {
+                lastDirection.y = Mathf.Sign(direction.z);
+            }
+        }
     }
 
     // Start is called before the first frame update
     void Start()
     {
         RegisterInputs();
+        lastDirection = -Vector2.one;
     }
 
     private void RegisterInputs()
@@ -43,6 +75,9 @@ public class PlayerEntity : MonoBehaviour, InputController.IPlayerControllerActi
         controls.PlayerController.Move.started += OnMove;
         controls.PlayerController.Move.performed += OnMove;
         controls.PlayerController.Move.canceled += OnMove;
+        controls.PlayerController.AttackInteract.started += OnAttackInteract;
+        controls.PlayerController.AttackInteract.performed += OnAttackInteract;
+        controls.PlayerController.AttackInteract.canceled += OnAttackInteract;
     }
 
     // Update is called once per frame
@@ -63,4 +98,22 @@ public class PlayerEntity : MonoBehaviour, InputController.IPlayerControllerActi
         return result;
     }
 
+    private IEnumerator AttackCoroutine()
+    {
+        isAttacking = true;
+
+        //Wait for start up frames
+        yield return new WaitForSeconds(attackStartUpFrames / 60f);
+        
+        attackHitbox.transform.localPosition = new Vector3(lastDirection.x * attackHitbox.size.x/2, lastDirection.y * attackHitbox.size.y/2, 0);
+        attackHitbox.enabled = true;
+        
+        //Active Frames
+        yield return new WaitForSeconds(attackActiveFrames / 60f);
+        attackHitbox.enabled = false;
+
+        //Recovery Frames
+        yield return new WaitForSeconds(attackRecoveryFrames / 60f);
+        isAttacking = false;
+    }
 }
