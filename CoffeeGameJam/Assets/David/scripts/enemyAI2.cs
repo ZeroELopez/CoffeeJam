e12ee3c43fd447d1643ef423039a3384b5499d90
oleend <davidpoleen@gmail.com>
2023-09-22 23:04:06 -0400

    Basic Enemy AI
    Please let me know what else is needed.

--- CoffeeGameJam/Assets/David/scripts/enemyAI2.cs
+++ CoffeeGameJam/Assets/David/scripts/enemyAI2.cs
@@ -20,11 +20,11 @@ public class enemyAI2 : MonoBehaviour
     {
         distance = Vector2.Distance(transform.position, player.transform.position);
         Vector2 direction = player.transform.position - transform.position;
-        direction.Normalize();
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        //direction.Normalize();
+        //float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
         transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
-        transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        //transform.rotation = Quaternion.Euler(Vector3.forward * angle);
         
 
         
