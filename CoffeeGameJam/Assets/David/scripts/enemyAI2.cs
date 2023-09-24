11b89e2a8b74ce457d15aa41138eeb855eaef1f4
oleend <davidpoleen@gmail.com>
2023-09-24 12:11:37 -0400

    Updates

--- CoffeeGameJam/Assets/David/scripts/enemyAI2.cs
+++ CoffeeGameJam/Assets/David/scripts/enemyAI2.cs
@@ -23,8 +23,12 @@ public class enemyAI2 : MonoBehaviour
         //direction.Normalize();
         //float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
-        //transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+
+        if (distance < 6)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+            //transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        }
         
 
         
