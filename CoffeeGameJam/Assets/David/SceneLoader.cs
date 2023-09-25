268c462de905692711a1bf90d0a732276069c90b
oleend <davidpoleen@gmail.com>
2023-09-25 16:53:43 -0400

    Title Screen Updates

---
+++ CoffeeGameJam/Assets/David/SceneLoader.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
